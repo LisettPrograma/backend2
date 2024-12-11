96ccbf86101b3b14531cb9a1b79c4df03bfe097d
brian <brian_coder_herrera@hotmail.com>
2024-12-10 23:10:59 -0300

    cambios para no usar base de datos

---
+++ gatos.js
@@ -0,0 +1,16 @@
+export const gatos = [
+  {
+    id: 1,
+    nombre: "Pedro",
+    edad: 3,
+    raza: "Enano",
+    sexo: "M",
+  },
+  {
+    id: 2,
+    nombre: "Juan",
+    edad: 5,
+    raza: "Alto",
+    sexo: "S",
+  },
+];
--- repository/gatitoRepository.js
+++ repository/gatitoRepository.js
@@ -1,54 +1,69 @@
-import {connect} from '../db.js'
-import { GatitoModel } from './models/gatitoModel.js';
-connect();
+// import { connect } from "../db.js";
+// import { GatitoModel } from "./models/gatitoModel.js";
+
+import { gatos } from "../gatos.js";
+
+// connect();
 
 export const getGatitoRepository = async () => {
-    try {
-        const gatitosObtenidos = await GatitoModel.find();
-        return gatitosObtenidos;
-    } catch (error) {
-        console.log(error);
-    }
-    
-}
+  try {
+    // const gatitosObtenidos = await GatitoModel.find();
+    // return gatitosObtenidos;
+    console.log(gatos);
+    return gatos;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getGatitoByIdRepository = async (id) => {
-    try {
-        const gatitoObtenido = await GatitoModel.findById(id);
-        return gatitoObtenido;
-    } catch (error) {
-        console.log(error);
-    }
-    
-}
+  try {
+    const gatitoObtenido = gatos.find((gato) => gato.id == id);
+    // const gatitoObtenido = await GatitoModel.findById(id);
+    return gatitoObtenido;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const postGatitoRepository = async (gatito) => {
-    try {
-        const gatitoAgregado = new GatitoModel(gatito);
-        await gatitoAgregado.save();
-        return gatitoAgregado;
-    } catch (error) {
-        console.log(error);
-    }
-    
-}
+  try {
+    // const gatitoAgregado = new GatitoModel(gatito);
+    // await gatitoAgregado.save();
+    const gatitoAgregado = gatito;
+    gatos.push(gatitoAgregado);
+    return gatitoAgregado;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const putGatitoRepository = async (id, gatito) => {
-    try {
-        const gatitoActualizado = await GatitoModel.findByIdAndUpdate(id, gatito, {new : true});
-        return gatitoActualizado;
-    } catch (error) {
-        console.log(error);
-    }
-    
-}
+  try {
+    // const gatitoActualizado = await GatitoModel.findByIdAndUpdate(id, gatito, {
+    //   new: true,
+    // });
+    // return gatitoActualizado;
+
+    const index = gatos.findIndex((gato) => gato.id == id);
+    gatos[index] = gatito;
+
+    return gatos[index];
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const deleteGatitoRepository = async (id) => {
-    try {
-        const gatitoBorrado = await GatitoModel.findByIdAndDelete(id);
-        return gatitoBorrado;
-    } catch (error) {
-        console.log(error);
+  try {
+    // const gatitoBorrado = await GatitoModel.findByIdAndDelete(id);
+    // return gatitoBorrado;
+    const index = gatos.findIndex((gato) => gato.id == id);
+    if (index !== -1) {
+      gatos.splice(index, 1);
     }
-    
-}
+    return gatos;
+  } catch (error) {
+    console.log(error);
+  }
+};
